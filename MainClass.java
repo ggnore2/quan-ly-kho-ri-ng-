a3c17b8181473888165acd43f58fd48818673d7f
L + Ratio <who asked>
2022-12-14 23:02:04 +0700

    _

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,32 @@
+import java.util.Date;
+
 public class MainClass {
     public static void main(String[] args) {
-
+        // BoPhanNhapGiaoDich.nhapGiaoDich(new GiaoDich("banh kem", "thuc an", 5, "mua",
+        // 5.0, new Date(), "kho A"));
+        // BoPhanNhapGiaoDich.nhapGiaoDich(new GiaoDich("keo", "thuc an", 5, "mua", 5.0,
+        // new Date(), "kho A"));
+        // BoPhanNhapGiaoDich.nhapGiaoDich(new GiaoDich("quat", "do dung", 5, "mua",
+        // 5.0, new Date(), "kho A"));
+        // BoPhanNhapGiaoDich.nhapGiaoDich(new GiaoDich("giuong", "do dung", 5, "mua",
+        // 5.0, new Date(), "kho A"));
+        // BoPhanNhapGiaoDich.nhapGiaoDich(new GiaoDich("mi an lien", "thuc an", 5,
+        // "mua", 5.0, new Date(), "kho A"));
+        // BoPhanNhapXuatKho.nhapKho(new Kho("kho A", "dia diem A", 25, 100));
+        // BoPhanNhapXuatKho.nhapHang(new Hang("banh kem", "thuc an", 5, " "));
+        // BoPhanNhapXuatKho.nhapHang(new Hang("keo", "thuc an", 5, " "));
+        // BoPhanNhapXuatKho.nhapHang(new Hang("quat", "do dung", 5, " "));
+        // BoPhanNhapXuatKho.nhapHang(new Hang("giuong", "do dung", 5, " "));
+        // BoPhanNhapXuatKho.nhapHang(new Hang("mi an lien", "thuc an", 5, " "));
+        // BoPhanNhapXuatKho.nhapKhoVaHang(new KhoVaHang("kho A", "dia diem A", "banh
+        // kem", "thuc an", 5));
+        // BoPhanNhapXuatKho.nhapKhoVaHang(new KhoVaHang("kho A", "dia diem A", "keo",
+        // "thuc an", 5));
+        // BoPhanNhapXuatKho.nhapKhoVaHang(new KhoVaHang("kho A", "dia diem A", "quat",
+        // "do dung", 5));
+        // BoPhanNhapXuatKho.nhapKhoVaHang(new KhoVaHang("kho A", "dia diem A",
+        // "giuong", "do dung", 5));
+        // BoPhanNhapXuatKho.nhapKhoVaHang(new KhoVaHang("kho A", "dia diem A", "mi an
+        // lien", "thuc an", 5));
     }
 }
